f45ebea5563be408109773e109856f687a883e1e
Zhengwei Wang <370363286@qq.com>
2017-12-20 14:57:46 +0800

    Add  Notes

--- setup.py
+++ setup.py
@@ -19,9 +19,9 @@ setup(
     packages=find_packages(
         exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),  #最后安装的时候需要排除测试相关的文件
     zip_safe=True,        # 出于性能考虑，通常可以把包打包成zip文件。如果包内没有数据文件、C扩展文件等就可以选择压缩
-    test_suite='tests',
-    tests_require=['pytest'],
-    cmdclass={'test': PyTest},
+    test_suite='tests',    # 指定测试套件的目录
+    tests_require=['pytest'],   # 测试需要安装的依赖，不需要使用 install_requires
+    cmdclass={'test': PyTest},   # 自定义了类命令
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Environment :: Web Environment',
